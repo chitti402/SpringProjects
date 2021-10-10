ce47ca3e5606572921fa0fa415464e8cc06a66fc
unknown <chitti402@gmail.com>
2021-10-10 11:40:54 +0530

    first commit

---
+++ SpringcloudserviceDemo/src/main/java/com/chitti/spring/SpringcloudserviceDemo/SpringcloudserviceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.chitti.spring.SpringcloudserviceDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudserviceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudserviceDemoApplication.class, args);
+	}
+
+}
---
+++ SpringcloudserviceDemo/src/test/java/com/chitti/spring/SpringcloudserviceDemo/SpringcloudserviceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chitti.spring.SpringcloudserviceDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudserviceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
