d3453ef5728c37419653865ac8696d3f52b6a1a4
unknown <chitti402@gmail.com>
2021-10-10 21:48:39 +0530

    spring config server and client setup

--- Springcloudclientservice/Springcloudclientservice/src/main/java/com/chitti/spring/Springcloudclientservice/SpringcloudclientserviceApplication.java
+++ Springcloudclientservice/Springcloudclientservice/src/main/java/com/chitti/spring/Springcloudclientservice/SpringcloudclientserviceApplication.java
@@ -1,10 +1,40 @@
 package com.chitti.spring.Springcloudclientservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@RestController
 public class SpringcloudclientserviceApplication {
+
+	@Value("${springcloud.servicedemo.provider.url}")
+	private String url;
+	
+	/*
+	 * @Autowired private RestTemplate restTemplate;
+	 */
+	
+	
+	@Autowired
+	RestTemplate restTemplate;
+	
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+	@GetMapping("/getPlan")
+	public String getPlan() {
+		String plans= restTemplate.getForObject(url, String.class);
+	
+	return plans;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringcloudclientserviceApplication.class, args);
