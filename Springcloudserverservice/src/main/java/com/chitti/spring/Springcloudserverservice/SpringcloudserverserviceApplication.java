04aa78b304f6ed3dc010c6c780dc4a075fa9ee68
unknown <chitti402@gmail.com>
2021-10-10 12:27:31 +0530

    added configuration project

---
+++ Springcloudserverservice/src/main/java/com/chitti/spring/Springcloudserverservice/SpringcloudserverserviceApplication.java
@@ -0,0 +1,13 @@
+package com.chitti.spring.Springcloudserverservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudserverserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudserverserviceApplication.class, args);
+	}
+
+}
---
+++ Springcloudserverservice/src/test/java/com/chitti/spring/Springcloudserverservice/SpringcloudserverserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chitti.spring.Springcloudserverservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudserverserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
