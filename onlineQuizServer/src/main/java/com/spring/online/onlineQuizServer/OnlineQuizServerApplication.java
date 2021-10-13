449b3e918bbcd88058537ea060bd27b6b4c4fb3e
unknown <chitti402@gmail.com>
2021-10-13 11:04:08 +0530

    adding client

--- onlineQuizServer/src/main/java/com/spring/online/onlineQuizServer/OnlineQuizServerApplication.java
+++ onlineQuizServer/src/main/java/com/spring/online/onlineQuizServer/OnlineQuizServerApplication.java
@@ -2,8 +2,10 @@ package com.spring.online.onlineQuizServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class OnlineQuizServerApplication {
 
 	public static void main(String[] args) {
---
+++ onlineQuizService1/src/main/java/com/spring/online/onlineQuizService1/OnlineQuizService1Application.java
@@ -0,0 +1,13 @@
+package com.spring.online.onlineQuizService1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineQuizService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineQuizService1Application.class, args);
+	}
+
+}
