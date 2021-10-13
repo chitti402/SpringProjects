eddca928532bf7da1b58be68172a065ffb950ca5
unknown <chitti402@gmail.com>
2021-10-13 10:37:36 +0530

    adding eureka server

---
+++ onlineQuizServer/src/main/java/com/spring/online/onlineQuizServer/OnlineQuizServerApplication.java
@@ -0,0 +1,13 @@
+package com.spring.online.onlineQuizServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineQuizServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineQuizServerApplication.class, args);
+	}
+
+}
---
+++ onlineQuizServer/src/test/java/com/spring/online/onlineQuizServer/OnlineQuizServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.online.onlineQuizServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineQuizServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
