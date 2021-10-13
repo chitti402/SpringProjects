cb64540e4029a268b9ca7f7bce8e764037d61550
unknown <chitti402@gmail.com>
2021-10-13 11:43:38 +0530

    onlinequizclient1

---
+++ onlineQuizClient1/src/main/java/com/spring/online/onlineQuizClient1/OnlineQuizClient1Application.java
@@ -0,0 +1,13 @@
+package com.spring.online.onlineQuizClient1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineQuizClient1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineQuizClient1Application.class, args);
+	}
+
+}
---
+++ onlineQuizService1/src/main/java/com/spring/online/controller/OnlineQuizController.java
@@ -0,0 +1,19 @@
+package com.spring.online.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/quiz")
+
+public class OnlineQuizController {
+	
+	
+@GetMapping("/service1")
+public String service() {
+	
+	return "Services1";
+}
+
+}
